7fdc4165137fd353c7ca241fc1d6e52877de1972
Luiz Felipe Bertoldi de Oliveira <luiz.oliveira@cwi.com.br>
2016-07-31 19:07:04 -0300

    Alterações.

--- www/wp-content/plugins/el-package/el-package.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
-* Plugin Name: Elefante Letrado - Pacote de Arquivos
-* Description: Pacote de Arquivos
-* Version: 1.0
-* Author: CWI Software - Luiz Felipe Bertoldi de Oliveira
-* Author URI: http://www.cwi.com.br
-*/
